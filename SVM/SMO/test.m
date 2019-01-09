6d2d957aff9ae33383973bc2eb0861ba542cd75d
lichongbin <lichongbin2004@126.com>
2019-01-09 15:31:50 +0800

    Accelerate the LoadIris function

--- SVM/SMO/test.m
+++ SVM/SMO/test.m
@@ -1,20 +1,26 @@
+% This is an example which test Sequential Minimization Optimization(SMO)
+% algorithm .
+% Two attributes are used as predicator: sepal length and sepal width.
+% Two categories: setosa and versicolor
+
 addpath('../../datasets/');
 
 [X, y] = LoadIris();
 
+% Get two attributes and the top 100 samples
 sepal_length = X(1:100, 1);
 sepal_width = X(1:100, 2);
 
 X = [sepal_length, sepal_width];
 y = y(1:100);
 
-
+% Positive and negative target values
 y(1:50) = 1;
 y(51:100) = -1;
 
 [alpha, b, TS] = SMO(X, y, 0.001, 0.001, 'L', [0, 1], 100, 0);
 
-d = 0.02;
+d = 0.01;
 % [x1Grid, x2Grid]= meshgrid(min(sepal_length):d:max(sepal_length), ...
 %                            min(sepal_width):d:max(sepal_width));
 
@@ -33,7 +39,7 @@ x2 = min(sepal_width):d:max(sepal_width);
 for i1 = x1
     for i2 = x2
         v = predict(X, y, alpha, b, [i1; i2]);
-        if v > -0.01 && v < 0.01
+        if v > -0.02 && v < 0.02
             plot(i1, i2, 'g.', 'MarkerSize', 10);
         end
     end
--- datasets/LoadIris.m
+++ datasets/LoadIris.m
@@ -1,10 +1,16 @@
 function [X, y] = LoadIris()
-
+% LOADIRIS Load Iris data set into the memory.
+% Iris data set can be download here:
+% http://archive.ics.uci.edu/ml/datasets.html
+%
 FID = fopen('iris.data');
 m = textscan(FID, '%f %f %f %f %s', 'Delimiter',',');
+fclose(FID);
+
+% the data type in column 1 to 4 is numeric
 X = cell2mat(m(1:4));
 categories = unique(m{5});
+y = zeros(size(X, 1), 1);
 for i=1:size(categories)
-    y((strcmp(m{5}, categories{i})) == 1) = i;
+    y( ( strcmp(m{5}, categories{i}) ) == 1 ) = i;
 end
-y = y';
